583c976ddacdb7483bea2e3264cd0208a89007e0
cl0cks4fe <cl0cks4fe@gmail.com>
2025-08-14 16:39:54 -0400

    add server

--- gadget/bootstrap/setup.sh
+++ gadget/bootstrap/setup.sh
@@ -6,11 +6,11 @@ echo "determining boot partition..."
 if [ -d "/boot/firmware" ]; then BOOT_DIR="/boot/firmware"; else BOOT_DIR="/boot"; fi
 echo "found boot partition: $BOOT_DIR"
 
-# Setup kernel layers
-echo "setting up kernel layers..."
+# Setup gadget mode
+echo "setting up gadget mode..."
 echo "dtoverlay=dwc2" | sudo tee -a "$BOOT_DIR/config.txt"
 sudo sed -i 's/rootwait /rootwait modules-load=dwc2,g_mass_storage /' $BOOT_DIR/cmdline.txt
-echo "kernel layers complete"
+echo "gadget mode complete"
 
 # Create USB image
 echo "creating and formatting usb image..."
@@ -19,27 +19,68 @@ sudo mkfs.vfat -F 32 -n "GADGET" /gadget.img
 echo "usb image created"
 
 # Install systemd service
-echo "installing systemd service"
-GADGET_URL="https://raw.githubusercontent.com/cl0cks4fe/shop/main/usb/bootstrap/files/gadget.service"
+echo "installing gadget.service"
+GADGET_SERVICE_URL="https://raw.githubusercontent.com/cl0cks4fe/shop/refs/heads/main/gadget/bootstrap/files/gadget.service"
 
 # Download the service file with error checking
-if ! sudo curl -fsSL "$GADGET_URL" -o /etc/systemd/system/gadget.service; then
+if ! sudo curl -fsSL "$GADGET_SERVICE_URL" -o /etc/systemd/system/gadget.service; then
     echo "error: failed to download service file from GitHub"
     exit 1
 fi
 
 # Verify the file was created
 if [ ! -f /etc/systemd/system/gadget.service ]; then
-    echo "error: service file not found after download"
+    echo "error: gadget service file not found after download"
     exit 1
 fi
 
+echo "installing gadget-server.service"
+GADGET_SERVER_SERVICE_URL="https://raw.githubusercontent.com/cl0cks4fe/shop/refs/heads/main/gadget/bootstrap/files/gadget-server.service"
+
+# Download the service file with error checking
+if ! sudo curl -fsSL "$GADGET_SERVER_SERVICE_URL" -o /etc/systemd/system/gadget-server.service; then
+    echo "error: failed to download service file from GitHub"
+    exit 1
+fi
+
+# Verify the file was created
+if [ ! -f /etc/systemd/system/gadget-server.service ]; then
+    echo "error: gadget-server service file not found after download"
+    exit 1
+fi
+
+echo "installing gadget server"
+GADGET_DIST="https://raw.githubusercontent.com/cl0cks4fe/shop/refs/heads/main/dist/gadget.zip"
+
+# Download the service file with error checking
+if ! sudo curl -fsSL "$GADGET_DIST" -o gadget.zip; then
+    echo "error: failed to download service file from GitHub"
+    exit 1
+fi
+
+# Verify the file was created
+if [ ! -f gadget.zip ]; then
+    echo "error: gadget-server zip not found after download"
+    exit 1
+fi
+
+sudo unzip gadget.zip -d /usr/local/bin/gadget
+sudo rm gadget.zip
+
+cd /usr/local/bin/gadget/server
+chmod +x scripts/transfer.sh
+python3 -m venv venv
+./venv/bin/pip install -r requirements.txt
+
 # reload and enable systemd
-echo "enabling systemd service..."
+echo "enabling systemd services..."
 sudo systemctl daemon-reload
+
 sudo systemctl enable gadget.service
-sudo systemctl start gadget.service
+sudo systemctl enable gadget-server.service
 
 echo "install successful, restarting in 10 seconds..."
+
 sleep 10
+
 sudo reboot
---
+++ gadget/server/scripts/transfer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo modprobe -r g_mass_storage
+
+sleep 5
+
+sudo mount /gadget.img /mnt -o loop
+
+rm -rf /mnt/*
+cp /usr/local/bin/gadget/server/upload/* /mnt/
+rm -f /usr/local/bin/gadget/server/upload/*
+
+sudo umount /mnt
+sudo modprobe g_mass_storage file=/gadget.img removable=1 stall=0
