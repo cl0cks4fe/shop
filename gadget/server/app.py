3d0c673f2de226277be7294eed238aaf382d38cd
cl0cks4fe <cl0cks4fe@gmail.com>
2025-09-12 16:17:26 -0400

    logger

--- gadget/server/app.py
+++ gadget/server/app.py
@@ -6,13 +6,25 @@ import threading
 import logging
 import requests
 
-app = Flask(__name__)
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'upload')
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M')
+DIRECTOR_URL = 'http://192.168.0.118:5000'
 
 registered = False
+
+app = Flask(__name__)
+logger = logging.getLogger(__name__)
+
+class LogHandler(logging.Handler):
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+    
+    def emit(self, record):
+        log_entry = self.format(record)
+        if registered:
+            requests.post(self.url, json={"log": log_entry})
+        else:
+            print(log_entry)
 
 def get_device_name():
     try:
@@ -55,23 +67,34 @@ def register():
     global registered
     while True:
         if not registered:
-            logger.info("attempting to register gadget...")
-            url = 'http://192.168.0.118:5000/register'
-
+            logger.info(f"attempting to register ({DIRECTOR_URL})...")
             try: 
-                response = requests.post(url, json={'id': DEVICE_NAME})
+                response = requests.post(f'{DIRECTOR_URL}/register', json={'id': DEVICE_NAME})
 
                 if response.status_code == 201:
                     registered = True
             except:
                 logger.error("attempt to register failed")
+        else:
+            try: 
+                response = requests.post(f'{DIRECTOR_URL}/register', json={'id': DEVICE_NAME})
+
+                if response.status_code != 200:
+                    registered = False
+            except:
+                logger.error("can't reach director")
     
         time.sleep(5)
 
 if __name__ == '__main__':
-    ping_thread = threading.Thread(target=register, daemon=True)
-    ping_thread.start()
+    log_handler = LogHandler(f'{DIRECTOR_URL}/logs')
+    log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    log_handler.setFormatter(log_formatter)
+    logger.addHandler(log_handler)
+    logger.setLevel(logging.ERROR)
+
+    register_thread = threading.Thread(target=register, daemon=True)
+    register_thread.start()
 
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-    app.run(host='0.0.0.0', port=8080, debug=True)
-
+    app.run(host='0.0.0.0', port=8080)
--- shop/server/app.py
+++ shop/server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import time
 import requests
 import threading
@@ -7,7 +7,7 @@ import logging
 app = Flask(__name__)
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M')
 
 class Gadget:
     def __init__(self, id, url, port):
@@ -21,25 +21,37 @@ gadgets = []
 def register():
     gadget_id = request.json.get('id')
     port = request.json.get('port', '8080')
-    logger.info(f'registering device: {gadget_id} ({request.remote_addr})')
+    logger.debug(f'registering device: {gadget_id} ({request.remote_addr})')
     if not gadget_id:
         return jsonify({"error": "no id provided"}, 400)
     if gadget_id not in [gadget.id for gadget in gadgets]:
         gadgets.append(Gadget(gadget_id, request.remote_addr, port))
     return jsonify({"message": "Gadget registered successfully", "gadget": gadget_id}), 201
 
+@app.route('/logs', methods=['POST'])
+def logs():
+    print(request.json)
+    return jsonify({"message": "ingested"}), 201
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', gadgets=gadgets)
+
 def ping_gadgets():
     global gadgets
     while True:
         alive = []
         for gadget in gadgets:
-            logger.info(f'pinging: {gadget.id} ({gadget.url})')
-            response = requests.get(f'http://{gadget.url}:{gadget.port}/ping')
+            logger.debug(f'pinging: {gadget.id} ({gadget.url})')
+            try:
+                response = requests.get(f'http://{gadget.url}:{gadget.port}/ping')
+            except:
+                pass
             if response.status_code == 200:
-                logger.info(f'success: {gadget.id} ({gadget.url})')
+                logger.debug(f'success: {gadget.id} ({gadget.url})')
                 alive.append(gadget)
             else:
-                logger.info(f'failure: {gadget.id} ({gadget.url})')
+                logger.debug(f'failure: {gadget.id} ({gadget.url})')
         gadgets = alive
         time.sleep(5)
 
@@ -48,4 +60,4 @@ if __name__ == '__main__':
     ping_thread.start()
 
     # Run the Flask app
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
